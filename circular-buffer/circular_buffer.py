cb6549677fe64763248d8de2ef7b5dc484ffbf79
lunaimaginea <luna@imaginea.com>
2017-11-29 16:05:02 +0530

    circular-buffer added

--- circular-buffer/circular_buffer.py
+++ circular-buffer/circular_buffer.py
@@ -8,4 +8,25 @@ class BufferEmptyException(Exception):
 
 class CircularBuffer(object):
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self._data = []
+
+    def read(self):
+        try:
+            return self._data.pop(0)
+        except IndexError:
+            raise BufferEmptyException
+
+    def write(self, new_item):
+        if len(self._data) == self.capacity:
+            raise BufferFullException
+        self._data.append(new_item)
+
+    def clear(self):
+        self._data = []
+
+    def overwrite(self, new_item):
+        self._data.append(new_item)
+        if len(self._data) > self.capacity:
+            self._data.pop(0)
+  
