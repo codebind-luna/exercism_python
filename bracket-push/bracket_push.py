ee147f6a9e96272865a35863a2be08c8a050201e
lunaimaginea <luna.das@imaginea.com>
2018-01-03 18:42:07 +0530

    changes

--- bracket-push/bracket_push.py
+++ bracket-push/bracket_push.py
@@ -1,2 +1,12 @@
-def check_brackets(input_string):
-    pass
+def check_brackets(string):
+    tokens = {'{': '}', '[': ']', '(': ')'}
+    stack = []
+
+    for char in string:
+        if char in tokens:
+            stack.append(tokens[char])
+        elif char in tokens.values():
+            if not stack or char != stack.pop():
+                return False
+
+    return not stack
--- reverse-string/reverse_string.py
+++ reverse-string/reverse_string.py
@@ -1,4 +1,4 @@
 def reverse(input=''):
     """rev_str = ''.join(input[i] for i in range(len(input)-1, -1, -1))
     return rev_str"""
-    input[::-1]
+    return input[::-1]
